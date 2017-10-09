5267ae0023104219b6a41e1f23fa4b54d5fe43cd
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-10-09 22:45:50 +0200

    end of session status

---
+++ README.md
@@ -0,0 +1,12 @@
+# todo list kata 
+
+We will practice this kata: the [ToDo list](https://github.com/liip/coding-dojo-katas/tree/master/02_todo_list).
+
+In our version, we will create a ToDo list manager.
+
+It will be created in an incremental fashion:
+
+- First deliverable: CLI version, in-memory storage
+- Second: add tags, categories, projects. add file persistence
+- Third: add multiple system users, add persistence to the cloud
+- Fourth: add multi-device sync (including conflict resolution)
---
+++ test/todo_list/core_test.clj
@@ -0,0 +1,22 @@
+(ns todo-list.core-test
+  (:require [clojure.test :refer :all]
+            [todo-list.core :refer :all]))
+
+(defn add [list name]
+  (cons name list))
+
+(defn check [list item-to-check]
+  (map
+    #(if
+       (= (:description %) item-to-check)
+       (conj % {:checked true})
+       %)
+    list))
+
+(deftest todo-list
+  (let [aDescription "buy milk" anotherDescription "buy sugar"]
+    (testing "An item can be added"
+      (is (= (add [aDescription] anotherDescription) [anotherDescription aDescription])))
+    (testing "An item can be checked"
+      (is (= (check [{:description aDescription}] aDescription) [{:description aDescription :checked true}]))
+      (is (= (check [{:description aDescription} {:description anotherDescription}] aDescription) [{:description aDescription :checked true} {:description anotherDescription}])))))
