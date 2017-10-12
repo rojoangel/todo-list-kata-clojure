c2dc0f8b8b01311aa9551f4ea3071f1691cdc444
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-10-12 12:00:18 +0200

    move production code to core ns

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,6 +1,37 @@
 (ns todo-list.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn item
+  ([description] (item description false))
+  ([description checked] {:description description :checked checked}))
+
+(defn todo-list [& items]
+  (reduce conj [] items))
+
+(defn- check-item [item]
+  (conj item {:checked true}))
+
+(defn- uncheck-item [item]
+  (conj item {:checked false}))
+
+(defn- has-desc? [item desc]
+  (= (:description item) desc))
+
+(defn- update-matches [list f item-desc]
+  (map
+    (fn [item]
+      (if (has-desc? item item-desc)
+        (f item)
+        item))
+    list))
+
+(defn add-item [list description]
+  (cons (item description) list))
+
+(defn remove-item [list item-desc]
+  (remove #(has-desc? % item-desc) list))
+
+(defn check [list item-desc]
+  (update-matches list check-item item-desc))
+
+(defn uncheck [list item-desc]
+  (update-matches list uncheck-item item-desc))
--- test/todo_list/core_test.clj
+++ test/todo_list/core_test.clj
@@ -2,42 +2,6 @@
   (:require [clojure.test :refer :all]
             [todo-list.core :refer :all]))
 
-(defn item
-  ([description] (item description false))
-  ([description checked] {:description description :checked checked}))
-
-(defn todo-list [& items]
-  (reduce conj [] items))
-
-(defn add-item [list description]
-  (cons (item description) list))
-
-(defn- check-item [item]
-  (conj item {:checked true}))
-
-(defn- uncheck-item [item]
-  (conj item {:checked false}))
-
-(defn- has-desc? [item desc]
-  (= (:description item) desc))
-
-(defn- update-matches [list f item-desc]
-  (map
-    (fn [item]
-      (if (has-desc? item item-desc)
-        (f item)
-        item))
-    list))
-
-(defn check [list item-desc]
-  (update-matches list check-item item-desc))
-
-(defn uncheck [list item-desc]
-  (update-matches list uncheck-item item-desc))
-
-(defn remove-item [list item-desc]
-  (remove #(has-desc? % item-desc) list))
-
 (deftest todo-list-test
   (let [aDescription "buy milk" anotherDescription "buy sugar"]
     (testing "An item can be added"
